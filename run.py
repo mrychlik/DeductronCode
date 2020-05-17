ee492df26e93d2ee5a84a5dd144c50a6987506c2
Marek Rychlik <marek@marekrychlik.com>
2020-05-17 14:47:42 -0700

    *** empty log message ***

---
+++ run.py
@@ -0,0 +1,13 @@
+from rychlik.deductron_pkg.test_deductron import *
+test_exact_model()
+test_large_model_1()
+test_comb_model_1()
+test_comb_model_2()
+
+from rychlik.deductron_pkg.test_metropolis_learning import *
+test_learning()
+
+# Run Tensorflow based training
+from rychlik.deductron_tf.test_deductron_tf import *
+
+test_learning_tf()
