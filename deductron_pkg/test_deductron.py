5260420ab0f33762996ace69156b914022dadd67
Marek Rychlik <marek@marekrychlik.com>
2020-05-17 11:02:42 -0700

    *** empty log message ***

--- deductron_pkg/test_deductron.py
+++ deductron_pkg/test_deductron.py
@@ -15,8 +15,25 @@ n_digits = 4
 def _testfun(nn, inputs, targets, descr):
     print(descr, nn(inputs).loss(targets).round(n_digits))
 
+def test_exact_model():
+    nn = WLangDecoderExact()   # Definitely always works
+    _testfun(nn, tiny_inputs, tiny_targets, "Tiny sample loss:   ");
+    _testfun(nn, small_inputs, small_targets, "Small sample loss:   ");
+    _testfun(nn, large_inputs, large_targets, "Large sample loss:   ");
+    _testfun(nn, comb_inputs, comb_targets, "Large sample loss:   ");
+    assert 0
+
+def test_large_model_1():
+    nn = WLangDecoderLargeModel1()    # Only works on large sample
+    _testfun(nn, tiny_inputs, tiny_targets, "Tiny sample loss:   ");
+    _testfun(nn, small_inputs, small_targets, "Small sample loss:   ");
+    _testfun(nn, large_inputs, large_targets, "Large sample loss:   ");
+    _testfun(nn, comb_inputs, comb_targets, "Large sample loss:   ");
+    assert 0
+
+
 def test_comb_model_1():
-    nn = WLangDecoderCombModel1()    # Works for all
+    nn = WLangDecoderCombModel1()    # Works except for tiny sample
     _testfun(nn, small_inputs, small_targets, "Small sample loss:   ");
     _testfun(nn, large_inputs, large_targets, "Large sample loss:   ");
     _testfun(nn, comb_inputs, comb_targets, "Large sample loss:   ");
@@ -31,11 +48,5 @@ def test_comb_model_2():
     _testfun(nn, comb_inputs, comb_targets, "Large sample loss:   ");
     assert 0
 
-def test_comb_model_2():
-    nn = WLangDecoderLargeModel1()    # Works for all
-    _testfun(nn, tiny_inputs, tiny_targets, "Tiny sample loss:   ");
-    _testfun(nn, small_inputs, small_targets, "Small sample loss:   ");
-    _testfun(nn, large_inputs, large_targets, "Large sample loss:   ");
-    _testfun(nn, comb_inputs, comb_targets, "Large sample loss:   ");
-    assert 0
+
 
