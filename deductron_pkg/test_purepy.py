6e2bfd691bc3ec801f3c672f35193e7ba5e13d3c
Marek Rychlik <marek@marekrychlik.com>
2020-05-17 11:24:27 -0700

    *** empty log message ***

---
+++ deductron_pkg/test_purepy.py
@@ -0,0 +1,14 @@
+import deductron_pkg.data
+import deductron_pkg.annealing_test
+
+def annealing_test(inputs, targets):
+    net, loss = QuantizedDeductron.train(
+        3, inputs, targets, beta_incr = 0.00001)
+    print(str(net))
+    return (net, loss)
+
+
+
+def test_answer():
+    annealing_test(small_inputs, small_targets)
+    assert 0 == 0
