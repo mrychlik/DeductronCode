0f9fd0e86d0f63b1de02d7afeec79f0e2d5ba580
Marek Rychlik <marek@marekrychlik.com>
2020-05-17 12:50:13 -0700

    *** empty log message ***

--- rychlik/__main__.py
+++ rychlik/__main__.py
@@ -1,18 +1,21 @@
 print('__file__={0:<35} | __name__={1:<20} | __package__={2:<20}'.format(__file__,__name__,str(__package__)))
 import rychlik.deductron_pkg as ded
-from rychlik.deductron_pkg.test_deductron import *
 
+
+# Run Tensorflow based trainint
+from rychlik.deductron_tf import *
+run_graph_training()
+
+
+from rychlik.deductron_pkg.test_deductron import *
 test_exact_model()
-test_large_model_1();
-test_comb_model_1();
-test_comb_model_2();
+test_large_model_1()
+test_comb_model_1()
+test_comb_model_2()
 
 from rychlik.deductron_pkg.test_metropolis_learning import *
 
-test_learning();
+test_learning()
 
-from rychlik.deductron_tf import *
-
-run_graph_training();
 
 
