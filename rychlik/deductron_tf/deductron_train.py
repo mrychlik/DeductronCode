2208c8d0f104c216d7c323ffd0010500b4678946
Marek Rychlik <marek@marekrychlik.com>
2020-05-17 12:43:07 -0700

    *** empty log message ***

--- rychlik/deductron_tf/deductron_train.py
+++ rychlik/deductron_tf/deductron_train.py
@@ -15,7 +15,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import io
 from datetime import datetime
-import data                     # Where the training data are
+import rychlik.deductron_pkg.data                     # Where the training data are
 
 #tf.enable_eager_execution() 
 #tf.executing_eagerly()        # => True
