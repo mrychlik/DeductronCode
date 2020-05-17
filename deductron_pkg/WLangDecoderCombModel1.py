b2783f5498a474af7315c4c6e0f13313fa85bc8c
Marek Rychlik <marek@marekrychlik.com>
2020-05-17 10:55:05 -0700

    *** empty log message ***

--- deductron_pkg/WLangDecoderCombModel1.py
+++ deductron_pkg/WLangDecoderCombModel1.py
@@ -2,25 +2,22 @@ import numpy as np
 from . DeductronBase import DeductronBase
 from . data    import *                # For sample inputs
 
-class WLangDecoderLargeModel1(DeductronBase):  
-    ''' Deductron trained on a 'stretched' sample of length 518. '''
+class WLangDecoderCombModel1(deductron_base.DeductronBase):  
+    '''Deductron trained on combined 'small' and 'stretched' samples of
+    total size 29 + 518.
+
+    '''
     def __init__(self):
-        super(WLangDecoderLargeModel1, self).__init__(beta = 10, shift = 0.5)
+        super(WLangDecoderCombModel1, self).__init__(beta = 10, shift = 0.5)
         self.W1 = np.array([
-            [ 1,  1, -1, -1,  1,  1],
-            [ 0,  0, -1,  1, -1,  1],
-            [-1,  1, -1,  1, -1,  1],
-            [-1, -1, -1, -1, -1, -1],
-            [ 1, -1, -1,  1,  0, -1],
-            [ 0, -1,  1,  0, -1,  0]
-            ]).astype(np.float32)
-
-        self.B1 = np.array([
-            [2], [1], [0], [3], [1], [0]
-            ]).astype(np.float32)
-
+            [ 0,  1,  1,  1,  1, -1],
+            [-1,  0,  1,  1, -1,  0],
+            [ 0, -1,  0, -1,  0, -1],
+            [ 1,  1, -1, -1,  0,  1],
+            [-1, -1,  0,  0,  0,  1],
+            [-1, -1, -1, -1, -1, -1]]).astype(np.float32)
+        self.B1 = np.array([[1], [0], [2], [2], [0], [0]]).astype(np.float32)
         self.W2 = np.array([
-            [ 1, -1,  1],
-            [-1,  1,  1]]).astype(np.float32)
-        self.B2 = np.array([[1], [1]]).astype(np.float32)
-    
+            [-1,  1, -1],
+            [ 1, -1, -1]]).astype(np.float32)
+        self.B2 = np.array([[2], [2]]).astype(np.float32)
