5beaee68567fc14b766b7f6bf57c2b6a00688617
Marek Rychlik <marek@marekrychlik.com>
2020-05-17 11:11:34 -0700

    *** empty log message ***

--- deductron_pkg/test_deductron.py
+++ deductron_pkg/test_deductron.py
@@ -19,7 +19,7 @@ def _test_net_template(nn):
     _test_template(nn, tiny_inputs, tiny_targets, "Tiny sample loss:   ");
     _test_template(nn, small_inputs, small_targets, "Small sample loss:   ");
     _test_template(nn, large_inputs, large_targets, "Large sample loss:   ");
-    _test_template(nn, comb_inputs, comb_targets, "Large sample loss:   ");
+    _test_template(nn, comb_inputs, comb_targets, "Combined sample loss:   ");
 
 def test_exact_model():
     nn = WLangDecoderExact()    # Definitely always works
