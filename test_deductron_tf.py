5b0bbc74ba416aff2f30cfe0076b3c94f7e26ca2
Marek Rychlik <marek@marekrychlik.com>
2020-05-23 12:22:46 -0700

    *** empty log message ***

--- test_deductron_tf.py
+++ test_deductron_tf.py
@@ -6,7 +6,10 @@ def test_tf_training():
     inputs  = data.comb_inputs
     targets = data.comb_targets
     n_memory   = 3                  # Num. of memory cells
-    ded_tf.train(inputs,targets,n_memory)
+    n_steps = 10000
+    loss_threshold = 0.1
+    learning_rate_adam = 0.01
+    ded_tf.train(inputs,targets,n_memory,n_steps,loss_threshold, learning_rate_adam)
 
 
 if __name__ == '__main__':
