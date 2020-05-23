d8dd2d5973b5f1e89cb6b1d415f8ae355979c4bc
Marek Rychlik <marek@marekrychlik.com>
2020-05-23 12:25:57 -0700

    *** empty log message ***

--- test_deductron_tf.py
+++ test_deductron_tf.py
@@ -6,9 +6,9 @@ def test_tf_training():
     inputs  = data.comb_inputs
     targets = data.comb_targets
     n_memory   = 3                  # Num. of memory cells
-    n_steps = 10000
-    loss_threshold = 0.1
-    learning_rate_adam = 0.01
+    n_steps = 10000                 # Num. epochs
+    loss_threshold = 0.1            # Loss goal
+    learning_rate_adam = 0.01       # Initial learning rate for Adam optimizer
     ded_tf.train(inputs,targets,n_memory,n_steps,loss_threshold, learning_rate_adam)
 
 
