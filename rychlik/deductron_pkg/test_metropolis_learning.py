c105cda4750f85e5bef6f60b72ae160f738516f7
Marek Rychlik <marek@marekrychlik.com>
2020-05-23 10:27:43 -0700

    *** empty log message ***

--- rychlik/deductron_pkg/test_metropolis_learning.py
+++ rychlik/deductron_pkg/test_metropolis_learning.py
@@ -1,5 +1,5 @@
-from . data import small_inputs, small_targets
-from . QuantizedDeductron import QuantizedDeductron
+from data import small_inputs, small_targets
+from deductron import QuantizedDeductron
 
 def _annealing_test(inputs, targets):
     net, loss = QuantizedDeductron.train(3, inputs, targets, beta_incr = 0.00001)
