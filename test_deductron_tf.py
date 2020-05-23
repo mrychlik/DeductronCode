18d132f74a5b4d3a54e2fd762785f79ab33f6fda
Marek Rychlik <marek@marekrychlik.com>
2020-05-23 13:29:51 -0700

    *** empty log message ***

--- test_deductron_tf.py
+++ test_deductron_tf.py
@@ -1,6 +1,7 @@
 # Run Tensorflow based training
-import deductron_tf as ded_tf
+from deductron_tf import DeductronTf
 import data
+import numpy as np
 
 def test_tf_training_tiny_data():
     inputs  = data.tiny_inputs
@@ -9,7 +10,10 @@ def test_tf_training_tiny_data():
     n_steps = 5000            # Num. epochs
     loss_threshold = 0.1      # Loss goal
     learning_rate_adam = 0.01 # Initial learning rate for Adam optimizer
-    ded_tf.train(inputs,targets,n_memory,n_steps,loss_threshold, learning_rate_adam)
+    ded = DeductronTf();
+
+    ret = ded.train(inputs,targets,n_memory,n_steps,loss_threshold, learning_rate_adam)
+    print("Outputs: {}".format(np.round(ret[0],0)));
 
 def test_tf_training_small_data():
     inputs  = data.small_inputs
@@ -18,7 +22,10 @@ def test_tf_training_small_data():
     n_steps = 20000           # Num. epochs
     loss_threshold = 0.1      # Loss goal
     learning_rate_adam = 0.01 # Initial learning rate for Adam optimizer
-    ded_tf.train(inputs,targets,n_memory,n_steps,loss_threshold, learning_rate_adam)
+    ded = DeductronTf();
+
+    ret = ded.train(inputs,targets,n_memory,n_steps,loss_threshold, learning_rate_adam)
+    print("Outputs: {}".format(np.round(ret[0],0)));
 
 def test_tf_training_big_data():
     inputs  = data.comb_inputs
@@ -27,11 +34,13 @@ def test_tf_training_big_data():
     n_steps = 600000           # Num. epochs
     loss_threshold = 0.01      # Loss goal
     learning_rate_adam = 0.001 # Initial learning rate for Adam optimizer
-    ded_tf.train(inputs,targets,n_memory,n_steps,loss_threshold, learning_rate_adam)
+    ded = DeductronTf();
 
+    ret = ded.train(inputs,targets,n_memory,n_steps,loss_threshold, learning_rate_adam)
+    print("Outputs: {}".format(np.round(ret[0],0)));
 
 if __name__ == '__main__':
-    test_tf_training_small_data()
+    test_tf_training_tiny_data()
     
 
 
