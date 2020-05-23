1740890c73f261fa2e5f571230c77d3825e5bdd8
Marek Rychlik <marek@marekrychlik.com>
2020-05-23 11:57:48 -0700

    *** empty log message ***

--- test_deductron_tf.py
+++ test_deductron_tf.py
@@ -1,8 +1,12 @@
 # Run Tensorflow based training
 from deductron_tf import train
+import data
 
 def test_tf_training():
-    train()
+    inputs  = data.comb_inputs
+    targets = data.comb_targets
+    n_memory   = 3                  # Num. of memory cells
+    train(inputs,outputs,n_memory)
 
 
 
