ae848738bca4e2a0ff4986db3e17b82a910a8784
Marek Rychlik <marek@marekrychlik.com>
2020-05-23 11:34:49 -0700

    *** empty log message ***

--- run.py
+++ run.py
@@ -1,14 +1,18 @@
 # Basic deductron tests
-from rychlik.deductron_pkg.test_deductron import *
-test_exact_model()
-test_large_model_1()
-test_comb_model_1()
-test_comb_model_2()
+import test_deductron as ded
+ded.test_exact_model()
+ded.test_large_model_1()
+ded.test_comb_model_1()
+ded.test_comb_model_2()
 
 # Train a model with Metropolis-Hastings
-from rychlik.deductron_pkg.test_metropolis_learning import *
-test_learning()
+import test_metropolis_learning as sim_annealing
+sim_annealing.test_learning()
+
+
 
 # Run Tensorflow based training
-from rychlik.deductron_tf.test_deductron_tf import *
-test_learning_tf()
+import deductron_tf as ded_tf
+ded_tf.train()
+
+
