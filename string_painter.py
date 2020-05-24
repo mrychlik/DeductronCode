cd8a552ffc86f557c87c84c0436e1117bd245060
Marek Rychlik <marek@marekrychlik.com>
2020-05-24 15:30:01 -0700

    *** empty log message ***

--- string_painter.py
+++ string_painter.py
@@ -4,15 +4,24 @@ import pylab
 from bbox import bbox;
 import os
 
-font_dir = "./fonts"
-font_size = 20
-font = ImageFont.truetype(os.path.join(font_dir, "bromello-Regular.ttf"),font_size)
-image = Image.new(mode='L', size=(600, 100), color='white')
-draw = ImageDraw.Draw(im = image)
-draw.fontmode = "1"
-text='Professor Rychlik can Bromello on occasions'
-draw.text(xy=(20, 40), text=text, fill='#000000', font = font)
-pixels = 255-np.asarray(image)
-box = bbox(pixels);
-draw.rectangle(box);
-image.save('my_test.png', 'PNG')
+DEFAULT_TEXT = 'Professor Rychlik can Bromello on occasions'
+DEFAULT_FONT_DIR = "./fonts"
+DEFAULT_FONT_SIZE = 20
+
+def draw_string(text=DEFAULT_TEXT, font_dir=DEFAULT_FONT_DIR, font_size = DEFAULT_FONT_SIZE):
+    font = ImageFont.truetype(os.path.join(font_dir, "bromello-Regular.ttf"),font_size)
+    image = Image.new(mode='L', size=(600, 100), color='white')
+    draw = ImageDraw.Draw(im = image)
+    draw.fontmode = "1"
+
+    draw.text(xy=(20, 40), text=text, fill='#000000', font = font)
+    pixels = 255-np.asarray(image)
+    box = bbox(pixels);
+    draw.rectangle(box);
+    image.save('my_test.png', 'PNG')
+    return (pixels[box[1]:box[3]][box[0]:box[2]], box )
+
+if __name__ == '__main__':
+    pixels,box = draw_string(DEFAULT_TEXT)
+    pylab.imshow(pixels)
+    pylab.show()
