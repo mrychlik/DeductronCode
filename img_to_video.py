8e72080564ba518bef8d2b794c66894d00f00b75
Marek Rychlik <marek@marekrychlik.com>
2020-05-25 16:59:48 -0700

    *** empty log message ***

---
+++ img_to_video.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def img_to_video(x, window):
+    z = np.zeros([x.shape[0],window])
+    # Pad with zeros on both ends
+    nrows = x.shape[0]
+    ncols = x.shape[1]
+    x = np.hstack([z,x,z])
+    lst = 2*window*[None]
+    for j in range(ncols-window):
+        lst[j] = x[:,j:(j+window)]
+    return lst
+
+
+
+def play_video(lst):
+    for j in range(len(lst)):
+        pylab.imshow(lst[j])
+        pylab.show()
