c1350536ac53a4e6dc47046457981926b66a7a29
Marek Rychlik <marek@marekrychlik.com>
2020-05-25 20:15:48 -0700

    *** empty log message ***

--- test_bromello_builder.py
+++ test_bromello_builder.py
@@ -2,7 +2,7 @@ from BromelloBuilder import *
 from img_to_video import *
 
 bb = BromelloBuilder();
-(unigrams,_) = bb.build_unigrams();
+unigrams,_ = bb.build_unigrams();
 six = unigrams[20]
 window = 3
 vid = img_to_video(six,window)
