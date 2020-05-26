96f2c524cbefd9984ad19e6a81e2a963fa9c93c4
Marek Rychlik <marek@marekrychlik.com>
2020-05-25 20:08:39 -0700

    *** empty log message ***

--- img_to_video.py
+++ img_to_video.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 
 def img_to_video(x, window):
     z = np.zeros([x.shape[0],window])
@@ -7,14 +8,18 @@ def img_to_video(x, window):
     nrows = x.shape[0]
     ncols = x.shape[1]
     x = np.hstack([z,x,z])
-    lst = 2*window*[None]
-    for j in range(ncols-window):
+    lst = (ncols+window)*[None]
+    for j in range(ncols+window):
         lst[j] = x[:,j:(j+window)]
     return lst
 
 
 
 def play_video(lst, delay=1):
-    for j in range(len(lst)):
-        pylab.imshow(lst[j])
-        pylab.show()
+    n = len(lst)
+    nr = np.ceil(np.sqrt(n))
+    nc = nr
+    for j in range(n):
+        plt.subplot(nr,nc,j)
+        plt.imshow(lst[j])
+    plt.show()
