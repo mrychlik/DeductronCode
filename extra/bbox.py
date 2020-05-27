0784ff7d79e1d89606e6d6897c26997863612e06
Marek Rychlik <marek@marekrychlik.com>
2020-05-27 09:54:19 -0700

    Removed extra folder for now

--- extra/bbox.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def bbox(arr):
-    '''Construct bounding box of a BW image given as a pixel array.'''
-    indw = np.nonzero(arr.any(axis=0))[0] # indices of non empty columns 
-    indh = np.nonzero(arr.any(axis=1))[0] # indices of non empty rows
-    return (indw[0],indh[0],indw[-1]+1,indh[-1]+1)
